806bc3b971d2d43672552cdd6f5a2bc7f329273f
Ichsannf <ichsanfauzi354@gmail.com>
2021-07-03 20:52:27 +0700

    first commit

---
+++ Assets/Scenes/MoveController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    Rigidbody2D move;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       move = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horiz ontal");
+        float vertikal = Input.GetAxisRaw("Vertical");
+        move.velocity = new Vector2(speed * horizontal, vertikal);
+       
+    }
+}
---
+++ Assets/Scenes/SmoothFollowMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowMouse : MonoBehaviour
+{
+    Vector3 mouse;
+    public float moveSpeed = 0.1f;
+    Rigidbody2D rb;
+    Vector2 position = new Vector2(0f, 0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse = Input.mousePosition;
+        mouse = Camera.main.ScreenToViewportPoint(mouse);
+        position = Vector2.Lerp(transform.position, mouse, moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(position);
+    }
+}
